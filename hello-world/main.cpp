4223f7a25ccad77c3da197e3ddd4c8ec57d43a46
BellaBustos <bellabustos@westernwyoming.edu>
2025-01-26 18:01:53 -0700

    first one

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,16 @@
 //
-// Name
-// Date
+// bella bustos
+// 1/25/25
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+```
+Ready! Set! Go!!
