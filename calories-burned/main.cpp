18aa6c7ab733b87077a15bf136394affeb6d6ec1
BellaBustos <bellabustos@westernwyoming.edu>
2025-02-22 22:34:51 -0700

    Update main.cpp

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,29 @@
 //
-// Name
-// Date
+// Bella Bustos
+// 2/22/25
 // Calories Burned Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    const double CALORIES_PER_MINUTE = 3.6;
+    const int MAX_MINUTES = 30;
+    const int INTERVAL = 5;
+
+    std::cout << "Calories burned on the treadmill:\n";
+    std::cout << "--------------------------------\n";
+    std::cout << "Minutes\tCalories Burned\n";
+    std::cout << "-------\t---------------\n";
+
+    for (int minutes = INTERVAL; minutes <= MAX_MINUTES; minutes += INTERVAL) {
+        double caloriesBurned = minutes * CALORIES_PER_MINUTE;
+        std::cout << std::setw(7) << minutes << "\t" 
+                  << std::fixed << std::setprecision(1) 
+                  << std::setw(15) << caloriesBurned << std::endl;
+    }
+
+    return 0;
+}
 
