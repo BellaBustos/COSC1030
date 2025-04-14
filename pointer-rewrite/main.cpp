c631b8bce31b783e1ec4d49a30f1b2308dcaf967
BellaBustos <bellabustos@westernwyoming.edu>
2025-04-13 21:31:26 -0600

    Update main.cpp

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,29 @@
 //
-// Name
-// Date
+// Bella Bustos
+// 4/10/24
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
+#include <iostream>
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 3;
+
+    std::cout << "Before: a = " << a << ", b = " << b << std::endl;
+    int result = doSomething(&a, &b);
+    std::cout << "After: a = " << a << ", b = " << b << std::endl;
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
 
