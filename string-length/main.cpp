78dbfc519335e74383d9c7b0c1828fcc7613ca66
BellaBustos <bellabustos@westernwyoming.edu>
2025-04-27 18:52:48 -0600

    Update main.cpp

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,32 @@
 //
-// Name
-// Date
+// Bella Bustos
+// 4/27/25
 // String Length Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
+
+// Function to count characters in a C-string
+int countChars(const char* str) {
+    int count = 0;
+    while (str[count] != '\0') {
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    const int SIZE = 100;
+    char input[SIZE];
+
+    cout << "Enter a string: ";
+    cin.getline(input, SIZE);
+
+    int numChars = countChars(input);
+
+    cout << "The number of characters in the string is: " << numChars << endl;
+
+    return 0;
+}
 
